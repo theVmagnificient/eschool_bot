05793d85986dad7f4ff70ab563299af8a9d4e4e8
theVmagnificient <42278932+theVmagnificient@users.noreply.github.com>
2018-08-14 17:55:44 +0300

    Added request and db modules
    DB is static
    Request handler will be created for each user in additional thread

---
+++ db.py
@@ -0,0 +1,56 @@
+from peewee import *
+
+
+db = SqliteDatabase('people.db')
+
+
+
+class Person(Model):
+    chatId = CharField()
+    login = CharField()
+    password = CharField()
+    userId = CharField()
+
+    class Meta:
+        database = db  # This model uses the "people.db" database.
+
+
+class DBInstance:
+    @staticmethod
+    def init():
+        db.connect()
+        db.create_tables([Person])
+
+    @staticmethod
+    def push(chatid, login, password, userid):
+        Person.create(chatId=chatid, login=login, password=password, userId=userid)
+
+    @staticmethod
+    def update(chatid, login = "", password = ""):
+        query = Person.select().where(Person.chatId == chatid)
+        if query.exists():
+            user = Person.get(Person.chatId == chatid)
+            if login != "":
+                user.login = login
+            if password != "":
+                user.password = password
+            user.save()
+
+    @staticmethod
+    def delete(chatid):
+        query = Person.select().where(Person.chatId == chatid)
+        if query.exists():
+            a = Person.get(Person.chatId == chatid).delete_instance()
+            print(a)
+
+    @staticmethod
+    def get(chatid):
+        query = Person.select().where(Person.chatId == chatid)
+        if query.exists():
+            return Person.get(Person.chatId == chatid)
+        else:
+            return "User doesn't exist"
+
+    @staticmethod
+    def close():
+        db.close()
---
+++ req.py
@@ -0,0 +1,86 @@
+import requests
+import hashlib
+from db import *
+
+
+
+
+
+
+def get_field_val(text, field, anchor = 0, mode="", endsym = ","):
+    if mode == "":
+        ind = text.find(field, anchor)
+        tmp = text.find(":", ind)
+        return ''.join([x for x in text[tmp + 1 : text.find(endsym, ind)] if (x != "\"" and x != "\'")])
+    # if anchor is the right bound
+    elif mode == "r":
+        ind = text.rfind(field, 0, anchor)
+        tmp = text.find(":", ind)
+        return''.join([x for x in text[tmp + 1: text.find(endsym, ind)] if (x != "\"" and x != "\'")])
+
+    else:
+        return "Incorrect mode"
+
+class EschoolConnectionHandler:
+
+    def __init__(self):
+        self._userID = None
+        self.s = requests.Session()
+        self.url = 'https://app.eschool.center/ec-server/login'
+        self.hash_pas = None
+
+    def login(self, log, password):
+        self.hash_pas = hashlib.sha256(password.encode()).hexdigest()
+        p = self.s.post(self.url, data={'username': log,
+                                        'password': self.hash_pas})
+
+        if p.status_code == 200:
+            return 0
+        else:
+            return p.status_code
+
+    def get_user_id(self):
+        if self._userID:
+            return self._userID
+        p = self.s.get("https://app.eschool.center/ec-server/state")
+        self._userID = get_field_val(p.text, "userId")
+        return self._userID
+
+    def get_per_id(self, per_name = "1 полугодие"):
+        p = self.s.get("https://app.eschool.center/ec-server/dict/periods2?year=2017")
+        anc = p.text.find(per_name, p.text.find("11 кл"))
+        id = get_field_val(p.text, "id", anchor=anc, mode="r")
+        return id
+
+    def get_avg_mark(self, subj_name="Алгебра", per_name="1 полугодие"):
+        p = self.s.get("https://app.eschool.center/ec-server/student/getDiaryUnits/?userId=" +
+                           self.get_user_id() + "&eiId=" + self.get_per_id(per_name))
+        anc = p.text.find(subj_name)
+        return get_field_val(p.text, "overMark", anc)
+
+
+a = EschoolConnectionHandler()
+
+log = input("Log: ")
+pas = input("Pas: ")
+a.login(log, pas)
+
+print(a.get_user_id())
+
+print(a.get_avg_mark())
+
+DBInstance.init()
+
+DBInstance.push("qwerty", log, a.hash_pas, a.get_user_id())
+
+a = DBInstance.get("qwerty")
+
+DBInstance.update("qwerty", "kek")
+
+b = DBInstance.get("qwerty")
+
+DBInstance.delete("qwerty")
+
+c = DBInstance.get("qwerty")
+
+DBInstance.close()
